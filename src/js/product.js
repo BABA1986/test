07ef8881c3373bc03f3570d1908067b83c3c7a31
Deepak Kumar <deepak.kumar@vzwdt.com>
2022-05-20 17:27:51 +0530

    Add model

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,16 @@
 (function ($) {
     "use strict";
-    var $products;
+    var products;
     $(document).ready(function () {
         console.log("product json called")
         $.getJSON("products/ctjProducts.json", function(productObjs){
+            products = productObjs
             var groupedProducts = $.fn.arrangeItemByCategory(productObjs);
             $.fn.appendTabs(groupedProducts);
         }).fail(function(){
             var products = "[{\"id\":1,\"pid\":1,\"title\":\"Paneer Tikka Roll\",\"catageory\":\"Rolls\",\"catageorySubtitle\":\"Snacks\",\"type\":1,\"img\":\"paneertikkarolls.png\",\"desc\":\"Soft cottage cheese marinated and skewered in tandoor mixed with lots of vegetables and sauces served on a crispy tawa parantha\",\"prices\":[{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":189}]},{\"id\":2,\"pid\":2,\"title\":\"Malai Chaap Roll\",\"catageory\":\"Rolls\",\"catageorySubtitle\":\"Snacks\",\"type\":1,\"img\":\"malaichaaproll.png\",\"desc\":\"Soya chaap marinated and skewered in tandoor mixed with lots of vegetables, cream and sauces served on a crispy tawa parantha.\",\"prices\":[{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":189}]},{\"id\":3,\"pid\":3,\"title\":\"Mutton Seek Roll\",\"catageory\":\"Rolls\",\"catageorySubtitle\":\"Snacks\",\"type\":0,\"img\":\"muttonseekroll.png\",\"desc\":\"[Chef's Special] Soft tendered lamb seekh marinated and skewered in tandoor mixed with lots of vegetables and sauces served on a crispy tawa parantha\",\"prices\":[{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":189}]},{\"id\":4,\"pid\":4,\"title\":\"Panner Tikka\",\"catageory\":\"Veg Tandoori\",\"catageorySubtitle\":\"Snacks\",\"type\":1,\"img\":\"pannertikka.png\",\"desc\":\"Fresh cottage cheese, marinated in fresh yogurt, gram flour and chilies skewered and grilled in the earthen oven - Classic, Malai, Achari\",\"prices\":[{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":299}]},{\"id\":5,\"pid\":5,\"title\":\"Mashroom Tikka\",\"catageory\":\"Veg Tandoori\",\"catageorySubtitle\":\"Snacks\",\"type\":1,\"img\":\"mashroom.png\",\"desc\":\"Fresh Mashroom, fresh cottage cheese, marinated in fresh yogurt, gram flour and chilies skewered and grilled in the earthen oven - Classic, Malai, Achari\",\"prices\":[{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":299}]},{\"id\":6,\"pid\":6,\"title\":\"Tandoori Chicken (With Butter)\",\"catageory\":\"Non Veg Barbeque\",\"catageorySubtitle\":\"Snacks\",\"type\":0,\"img\":\"tandoorichicken.png\",\"desc\":\"Chicken marinated in an ajwain flavored mixture of yoghurt, red chili, turmeric and garam masala, skewered and roasted over Earthen oven\",\"prices\":[{\"id\":1,\"type\":\"half\",\"desc\":\"\",\"price\":299},{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":499}]},{\"id\":7,\"pid\":7,\"title\":\"Afghani Chicken (With Butter)\",\"catageory\":\"Non Veg Barbeque\",\"catageorySubtitle\":\"Snacks\",\"type\":0,\"img\":\"afganichicken.png\",\"desc\":\"Chicken marinated in an cashew nut paste, black pepper and garam masala, skewered and roasted over Earthen oven\",\"prices\":[{\"id\":1,\"type\":\"half\",\"desc\":\"\",\"price\":329},{\"id\":1,\"type\":\"full\",\"desc\":\"\",\"price\":599}]}]"
             var productObjs = JSON.parse(products)
+            products = productObjs
             var groupedProducts = $.fn.arrangeItemByCategory(productObjs);
             $.fn.appendTabs(groupedProducts);
         });
@@ -39,6 +41,19 @@
             });
         }
 
+        /*Raise item Selection Model*/
+        $('body').on('click','.addProductButton',function(){
+            console.log("addProductButton")
+            var product = $(this).attr('data-id')
+            var productObj = JSON.parse(product)
+            $("#myModal").modal('show');
+        });
+
+        $('body').on('click','.model-close',function(){
+            console.log("addProductButton")
+            $("#myModal").modal('hide');
+        });
+
         /*Create Tabs Content*/
         $.fn.appendTabContent = function(tabItems, tabId, active) { 
             console.log("Function Called ---> appendTabContent ")
@@ -53,6 +68,7 @@
                     priceToDisplay = tabObj.prices.filter(v => v.type === "full");
                  }
 
+                 var jsonString = JSON.stringify(tabObj)
                  innerItems = innerItems + `<div class="col-lg-6"><div class="d-flex align-items-center"><img class="flex-shrink-0 img-fluid rounded" src="${tabObj.img}" alt="" style="width: 80px;">
                                         <div class="w-100 d-flex flex-column text-start border-bottom ps-4">
                                             <small class="d-flex justify-content-between">
@@ -62,7 +78,7 @@
                                                 </span>
                                                 <span>
                                                 <h6 class="text-primary">₹${tabObj.prices[0].price}</h6>
-                                                <a href="#" class="addButton">Add</a>
+                                                <a class="addProductButton" data-id='${jsonString}'>Add</a>
                                                 </span>
                                             </small>
                                         </div>
